194dce18f8f73c968ee58e05a55d900c2e7d771a
Natanael <taelima1997@gmail.com>
2020-06-07 17:52:00 -0300

    created seeder products

---
+++ database/seeds/ProductSeeder.js
@@ -0,0 +1,39 @@
+'use strict'
+
+/** @type {typeof import('@adonisjs/lucid/src/Lucid/Model')} */
+const Product = use('App/Models/Product')
+const Category = use('App/Models/Category')
+const producstData = require('./products.json')
+/*
+|--------------------------------------------------------------------------
+| ProductSeeder
+|--------------------------------------------------------------------------
+|
+| Make use of the Factory instance to seed database with dummy data or
+| make use of Lucid models directly.
+|
+*/
+
+/** @type {import('@adonisjs/lucid/src/Factory')} */
+
+class ProductSeeder {
+  async run () {
+    await Category.create({ name: 'carregador' })
+
+    producstData.map(p => {
+      try {
+        Product.create({
+          name: p.name,
+          amount: p.amount,
+          price: p.price,
+          category_id: 1
+        })
+      } catch (error) {
+        console.log(error)
+      }
+    }
+    )
+  }
+}
+
+module.exports = ProductSeeder
